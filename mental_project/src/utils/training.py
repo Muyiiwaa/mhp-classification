088a371a8759e1c4f16fedf48e07f29d000fee85
Muyiwa Abidoye <79062706+Muyiiwaa@users.noreply.github.com>
2025-07-05 17:44:06 +0000

    finished setting up the training and validation logic

--- mental_project/src/utils/training.py
+++ mental_project/src/utils/training.py
@@ -5,9 +5,12 @@ from tqdm import tqdm
 from torch import optim
 import torch
 from transformers import AutoModelForSequenceClassification, PreTrainedModel
-from typing import Optional, Tuple, Union, Dict
+from typing import Optional, Tuple, Union, Dict, List, Literal
 import logging
 from torch.utils.data import DataLoader
+from sklearn.metrics import f1_score, precision_score, recall_score
+from data_preparation import *
+from data_cleaning import *
 
 # configure logging
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
@@ -30,7 +33,7 @@ def load_model(model_uri: str) -> Tuple[Optional[PreTrainedModel], Optional[str]
         model_uri (str): _the model id on huggingface._
 
     Returns:
-        Tuple[Optional[PreTrainedModel], Optional[str]]: _MODEL, DEVICE_
+        Tuple[Optional[PreTrainedModel], Optional[str]]: _MODEL, DEVICE._
     """
     model = None
     device = None
@@ -46,10 +49,29 @@ def load_model(model_uri: str) -> Tuple[Optional[PreTrainedModel], Optional[str]
     return model, device
 
 
-def train_model(epoch: int, model:PreTrainedModel, data:DataLoader, device: str) -> Optional[Dict]:
-    metrics = None
-    learning_rate, optimizer, scheduler = get_params(model=model)
+def train_model(epoch: int, model:PreTrainedModel, 
+                data:DataLoader, device: Literal['cpu','cuda']) -> Tuple[List[float], List[float], float]:
+    """_This function performs the training/fine-tuning of the model on our mental health dataset.
+    It only contains the logic for training. It performs this operation on only one epoch and
+    returns the predictions, labels and value of the loss._
+
+    Args:
+        epoch (int): _The current epoch value._
+        model (PreTrainedModel): _The huggingface model object that we are finetuning on._
+        data (DataLoader): _the training dataset in form of a dataloader._
+        device (str): _the device type._
+
+    Returns:
+        Tuple[List[float], List[float], float]: _LABELS, PREDICTIONS, FINAL_LOSS._
+    """
+    _, optimizer, scheduler = get_params(model=model)
+
+    # track labels and predictions.
+    labels, predictions, loss_list = [], [], []
+    final_loss = 0.0
+    epoch = epoch + 1
     try:
+        model.train()
         train_batch = tqdm(data, desc= f'Training Epoch: {epoch}')
         for batch in train_batch:
             input_ids, attention_mask = batch['input_ids'].to(device), batch['attention_mask'].to(device)
@@ -58,7 +80,123 @@ def train_model(epoch: int, model:PreTrainedModel, data:DataLoader, device: str)
             loss = outputs.loss
 
             # back propagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # populate predictions and labels lists with the training output.
+            _,preds = torch.max(outputs.logits, 1) # get the actual model prediction.
+            if device == 'cpu':
+                labels.extend(label.detach().numpy())
+                predictions.extend(preds.detach().numpy())
+            else:
+                labels.extend(label.cpu().detach().numpy())
+                predictions.extend(preds.cpu().detach().numpy())
+            
+            # update loss list
+            loss_list.append(loss.item())
+
+            train_batch.set_postfix(loss=loss.item())
+
+        scheduler.step()
+
+        # get the final loss.
+        final_loss = sum(loss_list)/len(loss_list)
+        logger.info(msg=f'Completed training epoch: {epoch} with loss: {final_loss}')
+    except Exception as err:
+        logger.error(msg=f'Encountered error {err} while training epoch {epoch}')
+
+    return labels, predictions, final_loss
 
 
+# setup the validation loop
+def validate_model(epoch: int, model:PreTrainedModel, 
+                data:DataLoader, device: Literal['cpu','cuda']) -> Tuple[List[float], List[float], float]:
+    """_This function evaluates the trained.
+    It only contains the logic for evaluating the model on the test set. It performs this operation on only one epoch and
+    returns the predictions, labels and value of the loss._
+
+    Args:
+        epoch (int): _The current epoch value._
+        model (PreTrainedModel): _The huggingface model object that we are finetuning on._
+        data (DataLoader): _the test dataset in form of a dataloader._
+        device (str): _the device type._
+
+    Returns:
+        Tuple[List[float], List[float], float]: _LABELS, PREDICTIONS, FINAL_LOSS._
+    """
+    # track labels and predictions.
+    labels, predictions, loss_list = [], [], []
+    final_loss = 0.0
+    epoch = epoch + 1
+    try:
+        model.eval()
+        with torch.no_grad():
+            test_batch = tqdm(data, desc= f'testing Epoch: {epoch}')
+            for batch in test_batch:
+                input_ids, attention_mask = batch['input_ids'].to(device), batch['attention_mask'].to(device)
+                label = batch['label'].to(device)
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=label)
+                loss = outputs.loss
+
+                # populate predictions and labels lists with the testing output.
+                _,preds = torch.max(outputs.logits, 1) # get the actual model prediction.
+                if device == 'cpu':
+                    labels.extend(label.detach().numpy())
+                    predictions.extend(preds.detach().numpy())
+                else:
+                    label.extend(label.cpu().detach().numpy())
+                    predictions.extend(preds.cpu().detach().numpy())
+
+                # update loss list
+                loss_list.append(loss.item())
+                test_batch.set_postfix(loss=loss.item())
+            
+            final_loss = sum(loss_list)/len(loss_list)
+        logger.info(msg=f"Completed testing epoch: {epoch} with loss: {final_loss}")
+
+    except Exception as err:
+        logger.error(msg= f"Encountered error {err} while testing epoch {epoch}")
+
+    return label, predictions, final_loss
 
 
+def compute_metrics(label:List[float], predictions: List[float],
+                    final_loss:float, epoch: int) -> Optional[Dict]:
+    """_Helper function for computing metrics. It takes labels and 
+    predictions and returns key outputs._
+
+    Args:
+        label (List[float]): _The true labels._
+        predictions (List[float]): _The model predictions._
+        final_loss (float): _The loss from that training run._
+        epoch (int): _The current epoch being trained._
+
+    Returns:
+        Optional[Dict]: _the dictionary containing the metrics._
+    """
+    
+    f1 = f1_score(y_true=label, y_pred=predictions,average="weighted")
+    precision = precision_score(y_true=label, y_pred=predictions,average="weighted")
+    recall = recall_score(y_true=label, y_pred=predictions,average="weighted")
+
+    return {
+        'f1_score': f1,
+        'precision_score': precision,
+        'recall_score': recall,
+        'loss': final_loss,
+        'epoch': epoch +  1
+    }
+
+
+if __name__ == "__main__":
+    data, encoder = data_cleaner(data= download_data())
+    train_df, test_df = split_dataset(data = data)
+    train_loader, test_loader = final_dataloader(train_data=train_df,
+                                                 test_data=test_df)
+    model, device = load_model(model_uri="distilbert-base-uncased")
+    for epoch in range(5):
+        train_labels, train_preds, train_loss = train_model(epoch=epoch, model=model,
+                                                            data=train_loader,device=device)
+        test_labels, test_preds, test_loss = validate_model(epoch=epoch, model=model,
+                                                            data=test_loader,device=device)    
